7bd397d4b9cc51562b65f8f2cc8689307934f3f1
japygo <hyunnjinn@gmail.com>
2024-01-27 17:39:28 +0900

    포맷터 - Formatter

---
+++ src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.formatter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter formatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L); // Long 타입 주의
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
