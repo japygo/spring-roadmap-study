2b6cb482a082bcd0fed17ecb3b78accec4e76569
japygo <hyunnjinn@gmail.com>
2024-02-05 18:03:14 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백

--- src/main/java/hello/springtx/propagation/LogRepository.java
+++ src/main/java/hello/springtx/propagation/LogRepository.java
@@ -20,7 +20,7 @@ public class LogRepository {
         log.info("log 저장");
         em.persist(logMessage);
 
-        if (logMessage.getMessage().equals("로그예외")) {
+        if (logMessage.getMessage().contains("로그예외")) {
             log.info("log 저장시 예외 발생");
             throw new RuntimeException("예외 발생");
         }
