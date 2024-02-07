efbb58ad200364fa7153e5dd3dee8bfa6724fb48
japygo <hyunnjinn@gmail.com>
2024-02-07 18:45:30 +0900

    데코레이터 패턴 - 예제 코드3

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,8 +1,10 @@
 package hello.proxy.pureproxy.decorator;
 
+import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -11,16 +13,25 @@ class DecoratorPatternTest {
 
     @Test
     void noDecorator() {
-        RealComponent realComponent = new RealComponent();
+        Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
         client.execute();
     }
 
     @Test
     void decorator1() {
-        RealComponent realComponent = new RealComponent();
-        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
